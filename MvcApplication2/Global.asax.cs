f68f797051278da7b727f46e5a2804cb03c1ad5e
Steve Higgs <stevejhiggs@hotmail.com>
2012-06-24 13:19:47 +0100

    Initial commit

---
+++ MvcApplication2/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Linq;
+using System.Globalization;
+
+namespace MvcApplication2
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //replace standard razor view engine with the localisable one
+            ViewEngines.Engines.Remove(ViewEngines.Engines.OfType<RazorViewEngine>().First());
+            ViewEngines.Engines.Add(new LocalisedRazorViewEngine());
+
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("it");
+        }
+    }
+}
---
+++ MvcApplication2/LocalisedRazorViewEngine.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace MvcApplication2
+{
+    public class LocalisedRazorViewEngine : RazorViewEngine
+    {
+        public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            string overrideViewName = GetPrependedCultureName(viewName, Thread.CurrentThread.CurrentUICulture);
+            ViewEngineResult result = base.FindView(controllerContext, overrideViewName, masterName, useCache);
+
+            // If we're looking for a cultured view and couldn't find it try again without modifying the viewname
+            if (result == null || result.View == null)
+            {
+                result = base.FindView(controllerContext, viewName, masterName, useCache);
+            }
+            return result;
+        }
+
+        public override ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            string overrideViewName = GetPrependedCultureName(partialViewName, Thread.CurrentThread.CurrentUICulture);
+
+            ViewEngineResult result = base.FindPartialView(controllerContext, overrideViewName, useCache);
+
+            // If we're looking for a cultured view and couldn't find it try again without modifying the viewname
+            if (result == null || result.View == null)
+            {
+                result = base.FindPartialView(controllerContext, partialViewName, useCache);
+            }
+            return result;
+        }
+
+        private string GetPrependedCultureName(string viewName, CultureInfo culture)
+        {
+            string overrideViewName = "";
+            if (viewName.Contains("/"))
+            {
+                overrideViewName = string.Format("{0}/{1}{2}", viewName.Substring(0, viewName.LastIndexOf("/")), culture.Name, viewName.Substring(viewName.LastIndexOf("/")));
+            }
+            else
+            {
+                overrideViewName = string.Format("{0}/{1}", culture.Name, viewName);
+            }
+
+            return overrideViewName;
+        }
+    }
+}
